2078cddb72c4f8eb7f48cd47113d2f2839083570
Roxanne Lopez van Dooren <roxannelopez6@hotmail.com>
2014-10-22 09:50:41 +0200

    Added javascript file with login form control.

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+function checkform(myform){
+	if(myform.elements["username"].value == ""){
+		document.getElementById("usernameError").innerHTML = "Please provide your username*";
+		return false;
+	}
+	else if(myform.elements["password"].value == ""){
+		document.getElementById("passError").innerHTML = "Please provide your password*";		
+		return false;
+	}
+	return true;
+
+}
+
+function nospaces(object){
+	if(object.value.match(/\s/g) || object.value.match(/\./g)  ){
+        document.getElementById("spaceError").innerHTML = "No spaces allowed*";
+        object.value= "";
+    }
+}
+
+function reseting(){
+	document.getElementById("usernameError").innerHTML = "";
+	document.getElementById("passError").innerHTML = "";
+	document.getElementById("spaceError").innerHTML = "";
+}
