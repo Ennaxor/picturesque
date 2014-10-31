991100ad36164f926545e9358478973b884469aa
unknown <alberto.martinez.martinez.1@gmail.com>
2014-10-31 22:58:55 +0100

    Creado el cambio de estilo y modifiado algunos apartados de los css para que se ajusten bien

---
+++ js/style.js
@@ -0,0 +1,57 @@
+function changeStyle(name){
+	//alert(name);
+	var nombre= "Estilo ";
+	nombre=nombre+name;
+	var arrayLink = document.getElementsByTagName("link");
+	for (var i=0; i< arrayLink.length;i++){
+		if(arrayLink[i].getAttribute('rel')!=null && arrayLink[i].getAttribute('rel').indexOf('stylesheet') != -1 && arrayLink[i].getAttribute('media')!= 'print') {
+			if(arrayLink[i].title.toLowerCase() == nombre.toLowerCase()){
+				setCookie("style",name,365);
+				arrayLink[i].disabled=false;
+				//alert(document.cookie);
+			}
+			else{
+				arrayLink[i].disabled=true;
+			}
+		}
+		else{
+			
+		}
+	}
+}
+
+function getStringFromObject(object){
+	var name=object.id;
+	//alert(name);
+	changeStyle(name);
+}
+
+function cargarPagina(){
+	var aux= getCookie("style");
+	if(aux!=null){
+		//alert(aux);
+		var object = document.getElementById(aux);
+		//alert(object.value);
+		changeStyle(aux);
+	}
+}
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+    }
+    return "";
+}
