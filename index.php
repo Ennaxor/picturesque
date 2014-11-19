362543dba0bcea8aaf8486f69c3d68a77aff3203
Roxanne Lopez van Dooren <roxannelopez6@hotmail.com>
2014-11-19 19:29:01 +0100

    ..

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 				setcookie($cookie_name, '', time() - 42000, '/');
 			}
 
-            session_destroy();
+            session_destroy(); 
             $host = $_SERVER['HTTP_HOST'];
             $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
             $extra = 'index.php';
